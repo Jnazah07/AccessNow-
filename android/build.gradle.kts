4c3154ebf3bc425484e20052cd854c26ad4b3b17
Jnazah07 <jnazah07@gmail.com>
2026-08-07 22:37:39 +0400

    Fix Gradle project structure and plugin repositories

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,46 +1,4 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-}
-
-android {
-    compileSdk = 34
-    defaultConfig {
-        applicationId = "com.accessnow"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    namespace = "com.accessnow"
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.0")
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("org.webrtc:google-webrtc:1.0.32006")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
