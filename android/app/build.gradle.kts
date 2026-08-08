a1d8a65960e9f87bf21cf266638cdc85de98b092
Jnazah07 <jnazah07@gmail.com>
2026-08-08 22:37:08 +0400

    Fix WebRTC dependency mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,5 +28,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("org.webrtc:google-webrtc:1.0.32006")
+    implementation("io.getstream:stream-webrtc-android:1.1.1")
 }
